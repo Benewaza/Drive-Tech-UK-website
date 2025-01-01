9ccc274d6de083d95602894b96a03f643584d6fd
Ben Read <ben@mintcreative.com>
2025-01-01 18:30:43 +0000

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  output: 'static',
-  integrations: [tailwind()],
-  site: "https://github.com/Benewaza/Drive-Tech-UK-website",
-  base: '/<Drive-Tech-UK-website>/', // Base path for GitHub Pages
+  output: 'static', // Ensures a static build for GitHub Pages
+  integrations: [tailwind()], // Enables Tailwind CSS
+  site: "https://Benewaza.github.io/Drive-Tech-UK-website", // Deployed site URL
+  base: '/Drive-Tech-UK-website/', // Base path for GitHub Pages
 });
-
