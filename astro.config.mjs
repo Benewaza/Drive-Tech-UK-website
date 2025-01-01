ac95c21d6ea392f78b46df1bea253ba37099b5ea
Ben Read <ben@mintcreative.com>
2025-01-01 18:52:54 +0000

    update file paths for environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ export default defineConfig({
   output: 'static', // Ensures a static build for GitHub Pages
   integrations: [tailwind()], // Enables Tailwind CSS
   site: "https://Benewaza.github.io/Drive-Tech-UK-website", // Deployed site URL
-  base: '/Drive-Tech-UK-website/', // Base path for GitHub Pages
+  base: process.env.NODE_ENV === 'production' ? '/Drive-Tech-UK-website/' : '', // Conditionally set base path for production
 });
