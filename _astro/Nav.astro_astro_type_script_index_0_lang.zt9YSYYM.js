2b772233b9e7d49f4a4c3e893b29c280688beaa9
Ben Read <ben@mintcreative.com>
2025-01-01 18:31:20 +0000

    Updates

---
+++ _astro/Nav.astro_astro_type_script_index_0_lang.zt9YSYYM.js
@@ -0,0 +1,1 @@
+const t=document.getElementById("menu-toggle"),e=document.getElementById("mobile-menu");t.addEventListener("click",function(){e.classList.toggle("max-h-0"),e.classList.toggle("max-h-96")});
